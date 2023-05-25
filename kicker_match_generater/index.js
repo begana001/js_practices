33dbe461ae5677dd7742ca758e6f027832e70e3d
Begana Choi <76692843+begana001@users.noreply.github.com>
2023-05-25 21:07:31 +0200

    fix key name problem

--- kicker_match_generater/index.js
+++ kicker_match_generater/index.js
@@ -2,7 +2,9 @@
 const teams = ["hello world", "bananas", "le gang", "pandas", "new team"];
 teams.sort();
 const chunkSize = 2;
+const arraySliced = [];
 const kicker_match = {};
+const teamLength = teams.length;
 
 // team up by 2 teams
 splitTeams(teams, chunkSize);
@@ -11,10 +13,19 @@ function splitTeams(arr, chunkSize) {
   for (let i = 0; i < teams.length; i += chunkSize) {
     // 1. split by 2 teams
     let chunk = arr.slice(i, i + chunkSize);
-    // 2. push it to the kicker_match hash
-    kicker_match[`match ${i - 1}`] = chunk;
+    // 2. push it to the empty array
+    arraySliced.push(chunk);
+  }
+}
+console.log(arraySliced);
+
+// push sliced array to the hash
+function pushTeam(array) {
+  for (let i = 0; i < array.length; i++) {
+    kicker_match[`match ${i + 1}`] = array[i];
   }
 }
 
+pushTeam(arraySliced);
 // print the match list
 console.log(kicker_match);
