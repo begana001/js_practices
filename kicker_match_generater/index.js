d2908dd0425e66e228779979a75f00e6040099c4
Begana Choi <76692843+begana001@users.noreply.github.com>
2023-05-31 18:36:10 +0200

    built suffle function

--- kicker_match_generater/index.js
+++ kicker_match_generater/index.js
@@ -1,31 +1,11 @@
 // make an array of kicker teams
 const teams = ["hello world", "bananas", "le gang", "pandas", "new team"];
-teams.sort();
-const chunkSize = 2;
-const arraySliced = [];
-const kicker_match = {};
-const teamLength = teams.length;
 
-// team up by 2 teams
-splitTeams(teams, chunkSize);
-
-function splitTeams(arr, chunkSize) {
-  for (let i = 0; i < teams.length; i += chunkSize) {
-    // 1. split by 2 teams
-    let chunk = arr.slice(i, i + chunkSize);
-    // 2. push it to the empty array
-    arraySliced.push(chunk);
-  }
-}
-console.log(arraySliced);
-
-// push sliced array to the hash
-function pushTeam(array) {
-  for (let i = 0; i < array.length; i++) {
-    kicker_match[`match ${i + 1}`] = array[i];
-  }
+// shuffle array item
+function shuffleArray(array) {
+  return array.sort(() => 0.5 - Math.random());
 }
 
-pushTeam(arraySliced);
-// print the match list
-console.log(kicker_match);
+console.log(shuffleArray(teams));
+
+console.log([1, 2, 3, 4].sort(() => 0.5 - 0.33));
